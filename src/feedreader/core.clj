867ee6e2d8856c03e221c9cf48f99a0aec6adc2c
Ben <bcail@speedymail.org>
2021-08-19 20:18:24 -0400

    add parse-feed function

--- src/feedreader/core.clj
+++ src/feedreader/core.clj
@@ -1,7 +1,20 @@
 (ns feedreader.core
+  (:require [clojure.xml :as xml]
+            [clojure.java.io :as io])
+  (:import (java.io File))
   (:gen-class))
 
+(defn parse-feed
+  [data]
+  (let [input-stream (io/input-stream (.getBytes data))]
+    (for [x (:content ((:content (xml/parse input-stream)) 0)) :when (= :item (:tag x))]
+      {
+        :title ((:content ((:content x) 0)) 0)
+        :link ((:content ((:content x) 1)) 0)
+      }
+    )))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Feed Reader"
   [& args]
-  (println "Hello, World!"))
+  (println "Welcome to Feed Reader"))
--- test/feedreader/core_test.clj
+++ test/feedreader/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [feedreader.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest parse-feed-test
+  (testing "parse-feed"
+    ;https://www.rssboard.org/rss-specification
+    (let [data "<rss version=\"2.0\"><channel><title>Website</title><link>https://localhost</link><description>Description</description><item><title>Item 1 title</title><link>https://localhost/item1</link><pubDate>Fri, 18 Jun 2021 20:38:40 +0000</pubDate><comments>https://localhost/item1/comments</comments></item><item><title>Item 2 title</title><link>https://localhost/item2</link><pubDate>Fri, 25 Jun 2021 20:38:40 +0000</pubDate><comments>https://localhost/item2/comments</comments></item></channel></rss>"
+          parsed (parse-feed data)]
+      (is (= (first parsed) {:title  "Item 1 title", :link  "https://localhost/item1"}))
+      (is (= (nth parsed 1) {:title  "Item 2 title", :link  "https://localhost/item2"})))))
